4a73cc5837b92623ab465015c25e8093e8fe4228
adhulshaju <bossettans3@gmail.com>
2023-12-12 19:01:47 +0530

    updations

---
+++ loop_unrolling.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+// unrolled the for loop in program 1 printf("Hello\n");
+printf("Hello\n");
+printf("Hello\n");
+printf("Hello\n");
+printf("Hello\n");
+return 0;
+}
